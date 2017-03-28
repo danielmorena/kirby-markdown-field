b02e2a19d7962ba072ca5103635e961899a48d0e
SebastianEberlein <SebastianEberlein@users.noreply.github.com>
2017-03-28 15:30:39 +0200

    Add extended CodeMirror overflow fix

--- assets/js/editor.js
+++ assets/js/editor.js
@@ -2,40 +2,11 @@ var MarkdownField = function($field){
 	// only initialize once
 	if($field.data('markdown')) return;
 
-	function throttle(fn, threshhold, scope) {
-		threshhold || (threshhold = 250);
-		var last,
-				deferTimer;
-		return function() {
-			var context = scope || this,
-					now = +new Date,
-					args = arguments;
-			if (last && now < last + threshhold) {
-				clearTimeout(deferTimer);
-				deferTimer = setTimeout(function() {
-					last = now;
-					fn.apply(context, args);
-				}, threshhold);
-			} else {
-				last = now;
-				fn.apply(context, args);
-			}
-		};
-	};
-
 	var textarea = $field.get(0);
 	var codemirror = textarea.parentNode.querySelector('.codemirror');
 	var readOnly = codemirror.classList.contains('codemirror-is-readonly');
 	var disabled = codemirror.classList.contains('codemirror-is-disabled');
 	var hasCounter = textarea.parentNode.parentNode.querySelector('.field-counter');
-
-	// Fix overflow bug in Panel
-	function fixCodemirrorOverflow(){
-		$(codemirror).css('width', $field.parent().width());
-	}
-
-	fixCodemirrorOverflow();
-	window.addEventListener('resize', throttle(fixCodemirrorOverflow, 100));
 
 	// Initialize Codemirror
 	var options = {};
@@ -93,9 +64,96 @@ var MarkdownField = function($field){
 				});
 			}
 		});
-	}
+	};
 
 	$field.data('markdown', true);
+
+	// Fix overflow bug in Panel – this workaround is not ideal :(
+	function throttle(fn, threshhold, scope) {
+		threshhold || (threshhold = 250);
+		var last,
+				deferTimer;
+		return function() {
+			var context = scope || this,
+					now = +new Date,
+					args = arguments;
+			if (last && now < last + threshhold) {
+				clearTimeout(deferTimer);
+				deferTimer = setTimeout(function() {
+					last = now;
+					fn.apply(context, args);
+				}, threshhold);
+			} else {
+				last = now;
+				fn.apply(context, args);
+			}
+		}
+	};
+
+	function isDescendant(parent, child) {
+		var node = child.parentNode;
+		while (node != null) {
+			if (node == parent) {
+				return true;
+			}
+			node = node.parentNode;
+		}
+		return false;
+	};
+
+	var inModal = isDescendant(document.querySelector('.app > .modal'), textarea)
+	var field = textarea.parentNode.parentNode;
+	var fieldPadding = parseFloat($(field).css('padding-left'));
+
+	function fixOverflowBug(){
+		var widthFactor = 1;
+		var formWidth = document.querySelector('.mainbar .form').offsetWidth;
+		if (inModal) formWidth = document.querySelector('.modal .form').offsetWidth;
+
+		if (window.matchMedia('screen and (min-width:60em)').matches) {
+			if (field.classList.contains('field-grid-item-1-2') || field.classList.contains('field-grid-item-2-4')) {
+				widthFactor = 0.5;		
+			}
+			else if (field.classList.contains('field-grid-item-1-3')) {
+				widthFactor = 0.333;
+			}
+			else if (field.classList.contains('field-grid-item-2-3')) {
+				widthFactor = 0.666;
+			}
+			else if (field.classList.contains('field-grid-item-1-4')) {
+				widthFactor = 0.25;
+			}
+			else if (field.classList.contains('field-grid-item-3-4')) {
+				widthFactor = 0.75;
+			}
+			else if (field.classList.contains('field-grid-item-1-5')) {
+				widthFactor = 0.2;
+			}
+			else if (field.classList.contains('field-grid-item-2-5')) {
+				widthFactor = 0.4;
+			}
+			else if (field.classList.contains('field-grid-item-3-5')) {
+				widthFactor = 0.6;
+			}
+			else if (field.classList.contains('field-grid-item-4-5')) {
+				widthFactor = 0.8;
+			}
+		}
+
+		// add width to prevent text overflow
+		codemirror.style.width = ((formWidth * widthFactor) - fieldPadding) / 16 + 'em';
+	}
+
+	fixOverflowBug();	
+
+	setTimeout(function(){ 
+		// fix wrong cursor position
+		editor.refresh();
+		// fix modal position
+		if (inModal) $(document).trigger('keyup.center');
+	}, 100);
+
+	window.addEventListener('resize', throttle(fixOverflowBug, 100));
 };
 
 (function($) {
